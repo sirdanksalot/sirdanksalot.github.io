e617e35eb27f4d6653876ab933d00fd13c7c9148
sirdanksalot <kingpie321@gmail.com>
2025-03-31 16:37:01 -0400

    added gradebook

---
+++ Rupp_gradebook.js
@@ -0,0 +1,15 @@
+// TODO: Fetch data from the PostgreSQL database (to implement later)
+function fetchGradeData() {
+	// query PostgreSQL database and return grade data
+	console.log("Fetching grade data...");
+}
+// TODO: populate table
+function populateGradebook(data) {
+	// takes fetched data to populate table
+	console.log("populating gradebook with data:", data);
+}
+// TODO Remove this
+// call the stubs to demonstrate workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+//END Remove
